56010c3f029ccf396e7ef28beae3dd62a834c4bd
schooldev49 <Therobloxdeveloper@protonmail.com>
2022-06-07 21:15:07 -0400

    seg

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-(async() => {
-    await import('./app.mjs');
-})();
+import Server from 'bare-server-node'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+let http = require('http')
+let nodeStatic = require('node-static')
+let fs = require('fs')
+import custombare from './static/customBare.mjs'
+const bare = new Server('/bare/', '');
+
+const serve = new nodeStatic.Server('static/');
+const patronServe = new nodeStatic.Server('static/');
+const fakeServe = new nodeStatic.Server('fakeStatic/');
+
+const server = http.createServer();
+/*
+fs.readdir('/etc/letsencrypt/live', { withFileTypes: true }, (err, files) => {
+    if (!err)
+        files
+        .filter(file => file.isDirectory())
+        .map(folder => folder.name)
+        .forEach(dir => {
+            server.addContext(dir, {
+                key: fs.readFileSync(`/etc/letsencrypt/live/${dir}/privkey.pem`),
+                cert: fs.readFileSync(`/etc/letsencrypt/live/${dir}/fullchain.pem`)
+            });
+        });
+});
+*/
+server.on('request', (request, response) => {
+      if (bare.route_request(request, response)) { 
+        return true;
+      }
+    else if (custombare.isBare(request, response)) {
+        custombare.route(request,response); 
+    } else {
+      serve.serve(request, response);
+    }
+
+    
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bare.route_upgrade(req, socket, head))
+        return;
+
+    socket.end();
+});
+
+server.listen(443);
